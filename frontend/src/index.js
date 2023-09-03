afde9c20b9b017962bf6cc316281406c9deb1739
Pooria <p.asiabi@gmail.com>
2023-09-03 15:46:38 +0330

    Add Rating to product

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MainRoutes from './routes/MainRoutes';
 import {BrowserRouter} from 'react-router-dom'
-
+import "./assets/index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
