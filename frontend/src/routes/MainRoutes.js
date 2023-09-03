b234e0f6c74a784177e81efaa5811571ae17e3b2
Pooria <p.asiabi@gmail.com>
2023-09-03 16:05:35 +0330

    add single product page

--- frontend/src/routes/MainRoutes.js
+++ frontend/src/routes/MainRoutes.js
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import Product from '../pages/Product/Product'
 
 const MainRoutes = () => {
   return (
    <>
    <Header/>
    <Routes>
-    <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/product/:id' element={<Product/>}/>
    </Routes>
    <Footer/>
    </>
