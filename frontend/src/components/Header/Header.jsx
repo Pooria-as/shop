5fc17a7545209a016bd490b3ae96904199d60a10
Pooria <p.asiabi@gmail.com>
2023-09-03 14:09:55 +0330

    add logo to header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FaUser,FaShoppingCart } from "react-icons/fa";
-
+import Logo from "../../assets/logo.png"
 const Header = () => {
   return (
     <header>
         <Navbar variant='dark' bg='dark' expand="lg">
       <Container fluid className='ms-5'>
-        <Navbar.Brand href="/">Shop</Navbar.Brand>
+        <Navbar.Brand href="/">
+            <img src={Logo} alt="shop-logo"/>
+            Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className='ms-auto'>
